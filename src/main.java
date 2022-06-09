381cab7c0aec3477826bbfdcf708b0419bac5114
BuildTools <unconfigured@null.spigotmc.org>
2022-06-09 17:03:11 -0400

    Added Try/Catch block

--- src/main.java
+++ src/main.java
@@ -2,12 +2,39 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("You are in a land full of dragons. In front of you, \nyou see two caves. In one cave, the dragon is friendly \nand will share his treasure with you. The other dragon \nis greedy and hungry and will eat you on sight. \nWhich cave will you go into? (1 or 2)");
+        System.out.println("""
+                You are in a land full of dragons. In front of you,\s
+                you see two caves. In one cave, the dragon is friendly\s
+                and will share his treasure with you. The other dragon\s
+                is greedy and hungry and will eat you on sight.\s
+                Which cave will you go into? (1 or 2)""");
         Scanner s = new Scanner(System.in);
-        Integer i = s.nextInt();
-        if (i == 1)
-            System.out.println("You approach the cave... \nIt is dark and spooky... \nA large dragon jumps out in front of you! He opens his jaws and... \nGobbles you down in one bite!");
-        else if (i == 2)
-            System.out.println("You approach the cave... \nIt is dark and spooky... \nA large dragon jumps out in front of you! He opens his jaws and... \nSays hello and hands you some treasure");
+        int choice = 0;
+        while (choice == 0) {
+            boolean notNum = false;
+            try {
+                String i = s.next();
+                choice = Integer.parseInt(i);
+            } catch (NumberFormatException e){
+                notNum = true;
+                System.out.println("That is not a number!");
+            }
+            if (choice != 1 && choice != 2 && !notNum) {
+                choice = 0;
+                System.out.println("That is not a choice!");
+            }
+        }
+        if (choice == 1)
+            System.out.println("""
+                    You approach the cave...\s
+                    It is dark and spooky...\s
+                    A large dragon jumps out in front of you! He opens his jaws and...\s
+                    Gobbles you down in one bite!""");
+        else if (choice == 2)
+            System.out.println("""
+                    You approach the cave...\s
+                    It is dark and spooky...\s
+                    A large dragon jumps out in front of you! He opens his jaws and...\s
+                    Says hello and hands you some treasure""");
     }
 }
